e35bae677b6fe8425590378f7ef4f3e508ba665d
Terry <terryman42@gmail.com>
2024-05-30 07:22:18 -0600

    se realizo un template para la url raiz que mostrara todos las apps

--- hostal_project/apps/roles/urls.py
+++ hostal_project/apps/roles/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('roles/', views.listar_roles, name='listar_roles'),
     path('roles/crear/', views.crear_rol, name='crear_rol'),
     path('roles/<int:rol_id>/', views.ver_rol, name='ver_rol'),
-    path('roles/<int:rol_id>/actualizar/', views.actualizar_rol, name='actualizar_rol'),
-    path('roles/<int:rol_id>/eliminar/', views.eliminar_rol, name='eliminar_rol'),
+    path('roles/<int:rol_id>/actualizar/',
+         views.actualizar_rol, name='actualizar_rol'),
+    path('roles/<int:rol_id>/eliminar/',
+         views.eliminar_rol, name='eliminar_rol'),
 ]
